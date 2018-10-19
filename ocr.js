6e740642a536cee99ea88cbdfcd2e6744147bd49
cyruslk <cyrus7@hotmail.fr>
2018-10-19 10:14:40 -0400

    introducing the project

--- ocr.js
+++ ocr.js
@@ -1,35 +1,36 @@
 var tesseract = require('node-tesseract');
 var Scraper = require ('images-scraper')
   , google = new Scraper.Google();
-  
+
 
 tesseract.process('t_1.png', (err, text) => {
     if(err){
         return console.log("An error occured: ", err);
     }
-    console.log("here ---");
-    console.log(text);
-    const textArray = text.split(" ")
-    const textArrayLength = textArray.length;
-    console.log(textArray[0]);
-    
+    // console.log("here ---");
+    // console.log(text);
+    const textArray = text.split(" ");
+    // console.log(textArray);
+    // const textArrayLength = textArray.length;
+    console.log(textArray[1]);
+
     google.list({
-        keyword: `${textArray[0]}`,
+        keyword: `${textArray[1]}`,
         num: 1,
         detail: true,
         nightmare: {
-            show: true
+            show: false
         }
     })
     .then(function (res) {
-        console.log('----', res);
+        // console.log('----', res);
         console.log(res[0].thumb_url);
     }).catch(function(err) {
         console.log('err', err);
     });
-     
-    google.on('result', function (item) {
-        console.log('out', item);
-    });
-    
+
+    // google.on('result', function (item) {
+    //     console.log('out', item);
+    // });
+
 });
