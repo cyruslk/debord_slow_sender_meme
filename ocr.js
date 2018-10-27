09e1cf781114b9e1d885578da59fde2947a64c2c
cyruslk <cyrus7@hotmail.fr>
2018-10-27 17:01:24 -0400

    starting to send links into filesystem

--- ocr.js
+++ ocr.js
@@ -1,8 +1,10 @@
+var fs = require('file-system');
+
 var tesseract = require('node-tesseract');
 var Scraper = require ('images-scraper')
   , google = new Scraper.Google();
 
-
+let tracker = 0;
 
 
 tesseract.process('t_1.png', (err, text) => {
@@ -10,7 +12,6 @@ tesseract.process('t_1.png', (err, text) => {
         return console.log("An error occured: ", err);
     }
     const textArray = text.split(" ");
-
     google.list({
         keyword: `${textArray[tracker]}`,
         num: 1,
@@ -20,7 +21,14 @@ tesseract.process('t_1.png', (err, text) => {
         }
     })
     .then(function (res) {
+      
         console.log(res[tracker].thumb_url);
+        
+        
+        fs.writeFile('db/links.txt', res[tracker].thumb_url, function(err) {})
+        tracker = tracker+1;
+        console.log(tracker, "here");
+
 
     }).catch(function(err) {
         console.log('err', err);
