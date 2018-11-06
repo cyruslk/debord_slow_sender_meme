27a0266d575231d199d0a7d1a6e438011fbb5c0e
cyruslk <cyrus7@hotmail.fr>
2018-11-06 14:45:55 -0500

    build

---
+++ client/build/precache-manifest.bc86a1c2b7f3d7cd958226c1b6069487.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "829bf5ebd14114519dec",
+    "url": "/static/js/main.829bf5eb.chunk.js"
+  },
+  {
+    "revision": "5f5cfb93e85870ad266e",
+    "url": "/static/js/1.5f5cfb93.chunk.js"
+  },
+  {
+    "revision": "829bf5ebd14114519dec",
+    "url": "/static/css/main.25ed9f3b.chunk.css"
+  },
+  {
+    "revision": "09220919ed0647fc797ee5c4e1972fb0",
+    "url": "/index.html"
+  }
+];
